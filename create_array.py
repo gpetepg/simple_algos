eb3725dd5a693ea10d050ac84846649d92d28d5c
Ty <tylereguo@gmail.com>
2019-07-25 20:23:47 -0700

    Fixes

--- create_array.py
+++ create_array.py
@@ -1,13 +1,4 @@
 import random
 
-def create_array(mx, sz, sort=False, is_set=False):
-    if is_set:
-        if sort:
-            return sorted(set([random.randint(1, mx) for x in range(0, sz)]))
-        else:
-            return list(set([random.randint(1, mx) for x in range(0, sz)]))
-    else:
-        if sort:
-            return sorted([random.randint(1, mx) for x in range(0, sz)])
-        else:
-            return [random.randint(1, mx) for x in range(0, sz)]
+def create_array(mx, sz):
+    return [random.randint(0, mx) for x in range(0, sz)]
